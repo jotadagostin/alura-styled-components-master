39644a27471a6beafd9e550734abfa1c68478a44
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-06-08 09:39:02 +0200

    update colors
    icone theme, black and white

--- src/Components/ImageFilter.js
+++ src/Components/ImageFilter.js
@@ -4,15 +4,15 @@ import outros from "../assets/images/outros.svg";
 import saude from "../assets/images/saude.svg";
 import transporte from "../assets/images/transporte.svg";
 import utilidades from "../assets/images/utilidades.svg";
-import { Icone } from "../Components/UI";
+import { IconeTema } from "../Components/UI";
 
 export default (type) => {
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="restaurante" />,
-    Utilidades: <Icone src={utilidades} alt="utilidades" />,
-    Saude: <Icone src={saude} alt="Saude" />,
-    Transporte: <Icone src={transporte} alt="transporte" />,
-    default: <Icone src={outros} alt="Outro" />,
+    Restaurante: <IconeTema src={alimentacao} alt="restaurante" />,
+    Utilidades: <IconeTema src={utilidades} alt="utilidades" />,
+    Saude: <IconeTema src={saude} alt="Saude" />,
+    Transporte: <IconeTema src={transporte} alt="transporte" />,
+    default: <IconeTema src={outros} alt="Outro" />,
   };
 
   return Images[type] || Images.default;
