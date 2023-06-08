14016976feee10594393b1dc53fa8210da19ff85
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-06-08 09:09:52 +0200

    update colors

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,20 @@
+import {
+  fundoClaro,
+  conteudoClaro,
+  textoFundoclaro,
+  fundoEscuro,
+  conteudoEscuro,
+  textoFundoEscuro,
+} from "./variaveis";
+
+export const temaClaro = {
+  body: fundoClaro,
+  inside: conteudoClaro,
+  text: textoFundoclaro,
+};
+
+export const temaEscuro = {
+  body: fundoEscuro,
+  inside: conteudoEscuro,
+  text: textoFundoEscuro,
+};
